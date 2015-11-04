80502eb597a30056a4fc2de2ca0b8f352902f534
cliff0891 <yuyunc@student.swosu.edu>
2015-11-04 09:21:49 -0600

    hello!

--- src/listing4_16.java
+++ src/listing4_16.java
@@ -1,11 +1,8 @@
 
 public class listing4_16 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+	
 
 	}
 
