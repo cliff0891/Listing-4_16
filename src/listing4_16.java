84c0a187558d52678d9e272b9927e4355f4b985e
cliff0891 <yuyunc@student.swosu.edu>
2015-11-04 09:32:50 -0600

    working code

--- src/listing4_16.java
+++ src/listing4_16.java
@@ -3,7 +3,15 @@ public class listing4_16 {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
 	
-
+		// A was 65
+		// Z was 90
+		// we want to start at 65 and go to 65 +
+		// A is 65 
+		// B is going to be 25
+		int number =(int)(65 + Math.random()* (25+1));
+		System.out.println(number);
+		char letter = (char)number;
+		System.out.println(letter);
 	}
 
 }
